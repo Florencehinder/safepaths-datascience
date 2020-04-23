8d3591f30b504f63c6c9e0ce6057158f5ccee9aa
philrouge <andrea.nuzzo@me.com>
2020-04-23 17:30:28 -0400

    Utils function now recognize csv vs json and ingest both automatically. Added data sources.

--- import_data.py
+++ import_data.py
@@ -1,21 +1,10 @@
 from utils import *
 
-log_filename = f"logs/{('_'.join([str(t) for t in time.gmtime()[0:3]]))}.log"
-os.makedirs(os.path.dirname(log_filename), exist_ok=True)
-
 with open('data_sources.json', 'r') as myfile:
     data=myfile.read()
 
 sources = json.loads(data)
-url = sources['Nations']['US']['National']['JHU CSSE']
+url = sources['Nations']['Italy']['Regions']['Department of Civil Protection']
 
 
-if re.search('.csv', url):
-    try:
-        check_GPS_csv(url)
-    except ValueError as e:
-        with open(log_filename, 'a') as log:
-            log.writelines(('-'.join([str(t) for t in time.gmtime()[3:6]]))+str(e))
-            e
-
-    build_GPS_data_csv(url)
+build_GPS_data(url)
--- utils.py
+++ utils.py
@@ -9,31 +9,53 @@ import urllib3
 
 http = urllib3.PoolManager()
 
-def check_GPS_csv(url):
-    
+log_filename = f"logs/{('_'.join([str(t) for t in time.gmtime()[0:3]]))}.log"
+os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+
+def check_availability(url):
     response=http.request('GET', url)
     
     if response.status==500:
         raise ValueError('Connection failed.')
+
+    return response
+
+def check_data_type(url):
+
+    response = check_availability(url)
+
+    try:
+        if re.search('.csv', url):
+            temp = list(csv.DictReader(response.data.decode('utf-8').splitlines()))
+        elif re.search('.json', url):
+            temp = json.loads(response.data)
+    except ValueError as e:
+        with open(log_filename, 'a') as log:
+            log.writelines(('-'.join([str(t) for t in time.gmtime()[3:6]]))+str(e))
+            e
+    return temp
+
+def check_GPSdata(url):
+
+    temp = check_data_type(url)
     
     relong = re.compile("long", re.IGNORECASE)
-    relat = re.compile("lat", re.IGNORECASE)
-    
-    temp = list(csv.DictReader(response.data.decode('utf-8').splitlines()))
-    
+    relat = re.compile("lat", re.IGNORECASE)  
+
     if list(filter(relat.match, list(temp[0].keys()))) and list(filter(relong.match, list(temp[0].keys()))):
         print('GPS data found')
     else:
         raise ValueError('No GPS data found. \nColumn names are:', temp[0])
 
+    return temp
 
-def build_GPS_data_csv(url, filename='gps_data.csv'):
+def build_GPS_data(url, filename='gps_data.csv'):
+    
     with open(filename, "w", newline='') as result:
         writer = csv.DictWriter(result, ['latitude','longitude'])
         writer.writeheader()
 
-        response = http.request('GET', url)
-        temp = list(csv.DictReader(response.data.decode('utf-8').splitlines()))
+        temp = check_data_type(url)
 
         for d in temp[:10]:      #limit to 10 rows for testing purpose
             for key in list(temp[0].keys()):
