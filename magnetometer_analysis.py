fb6ed8620c129532d10da927d3760e8e09457285
philrouge <andrea.nuzzo@me.com>
2020-05-04 15:24:11 -0400

    Added true distances and correlation

--- magnetometer_analysis.py
+++ magnetometer_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import math
 import datetime
 import os
@@ -20,14 +20,53 @@ phone2 = pd.read_json(data_folder/'mgm_b.json')
 phone1['timestamp'] = phone1['timestamp'].dt.round('min')
 phone2['timestamp'] = phone2['timestamp'].dt.round('min')
 
+def get_distance(ts):    
+    #if outside the test window, return None
+    if ts < datetime.datetime(2020, 5, 2, 10, 54, 30):
+        return 150
+    if ts < datetime.datetime(2020, 5, 2, 10, 56, 25):
+        return 126
+    if ts < datetime.datetime(2020, 5, 2, 10, 58, 25):
+        return 80
+    if ts < datetime.datetime(2020, 5, 2, 11, 0, 24):
+        return 51
+    if ts < datetime.datetime(2020, 5, 2, 11, 2, 21):
+        return 22
+    if ts < datetime.datetime(2020, 5, 2, 11, 4, 22):        
+        return 9
+    if ts < datetime.datetime(2020, 5, 2, 11, 6, 28):
+        return 0 
+   
+    if ts < datetime.datetime(2020, 5, 2, 11, 14, 0):
+        return 138 + 182
+    if ts < datetime.datetime(2020, 5, 2, 11, 17, 20):
+        return 138 + 161
+    if ts < datetime.datetime(2020, 5, 2, 11, 19, 0):
+        return 138 + 126
+    if ts < datetime.datetime(2020, 5, 2, 11, 21, 0):
+        return 138 + 61
+    if ts < datetime.datetime(2020, 5, 2, 11, 26, 35):
+        return 138 + 61
+      
+
+_dcoll = []
+for index,row in phone2.iterrows():
+    _d = get_distance(row['timestamp'])
+    _dcoll.append(_d)
+phone2['truth'] = _dcoll
+
+
 a = phone1.set_index('timestamp').rolling('1min').mean()#.assign(device=1).head()
 b = phone2.set_index('timestamp').rolling('1min').mean()#.assign(device=2).head()
 
 
 ## Merge the two phones
 df = a.merge(b, on='timestamp', how='outer', suffixes=('_a','_b')).sort_index()
+df['time_elapsed'] = pd.TimedeltaIndex(df.index - df.index[0], unit='m') / np.timedelta64(1, 'm')
+df.head()
 
-## Apply euclidian distance
+
+## Apply distances
 def distance_3d(row, metric='euclidean'):
     """
     Calculates the 3d distance rowwise in a dataframe. 
@@ -45,8 +84,26 @@ def distance_3d(row, metric='euclidean'):
 
     return(dist[0])
 
-df['distance a_b'] = df.apply(distance_3d, axis=1)
-df['time_elapsed'] = pd.TimedeltaIndex(df.index - df.index[0], unit='m') / np.timedelta64(1, 'm')
+metrics = ['braycurtis', 'canberra', 'chebyshev', 'cityblock',
+    'correlation', 'cosine', 'dice', 'euclidean', 'hamming',
+    'jaccard', 'jensenshannon', 'kulsinski', 'matching',
+    #'mahalanobis', 
+    'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean',
+    'sokalmichener', 'sokalsneath', 'sqeuclidean', 'yule']
 
-df.plot(kind='scatter',x='time_elapsed',y='distance a_b',color='red')
+for metric in metrics:
+    df[metric] = df.apply(distance_3d, metric=metric, axis=1)
+    df.plot(kind='scatter',x='truth',y=metric,color='blue')
+    plt.show()
+
+## Plot ground truth (?)
+df.plot(kind='scatter',x='time_elapsed',y='truth',color='blue')
+
+## correlation with truth?
+plot_data = df.query('truth<150').\
+    loc[:,['truth','braycurtis', 'canberra', 'chebyshev', 'cityblock',
+    'correlation', 'cosine', 'euclidean', 'minkowski']]
+
+plot_data.corr().loc[:,['truth']].style.background_gradient(cmap='RdYlBu').set_precision(3)
+
 df.to_csv('test_dist.csv')
